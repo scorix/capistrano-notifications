8c1c9a039d5e35ebeaa0800a8d1c9384da486e2b
scorix <scorix@gmail.com>
2015-02-13 12:44:39 +0800

    fix arguments error

--- lib/capistrano/notifications/adapter/base.rb
+++ lib/capistrano/notifications/adapter/base.rb
@@ -11,13 +11,13 @@ module Capistrano
           instance
         end
 
-        def notify(members = [], message = '')
-          Array.wrap(members).each { |member| send_notification(member, message) }
+        def notify(members = [], message = '', *_extra_args)
+          Array.wrap(members).each { |member| send_notification(member, message, *_extra_args) }
         end
 
         protected
 
-        def send_notification(_member, _message)
+        def send_notification(_member, _message, *_extra_args)
           raise NoMethodError, 'send_notification is not implemented.'
         end
 
